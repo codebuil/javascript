4de3a1f05054281c7668f421fd87a7489d65dc05
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-09-11 13:16:09 +0100

    wasm sample

--- lcase.c
+++ lcase.c
@@ -1,32 +1,21 @@
-//https://wasdk.github.io/WasmFiddle/
-#include<string.h>
-#include<stdio.h>
-  char i[]={"HELLO WORLD...\n"};
-char *hello(){ 
-	char *ii;
-	ii=i;
-	return ii;
-}
+char i[1024]="HELLO WORLD\n\0";
 char *lcases(){
 	int ff=0;
 	char c=0;
 	char *cc;
-	for(ff=0;ff<strlen(i);ff++){
+     do{
 		c=i[ff];
-		if(c>65 && c<91){
+		if(c==0)goto lcaseescape;
+		if(c>64 && c<65+27){
 			c=c+32;
 			i[ff]=c;
 		}
-	}
+		ff++;
+		
+	}while(1);
+	lcaseescape:
 	cc=i;
 	return cc;
 }
 
 
-int main(){
-	int d=0;
-	int e=0;
-	printf("\033[42;37m\n");
-	puts(lcases());
-	return 0;
-}

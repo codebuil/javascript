6f8ee25e62937499b72fcac435cedbdb50e171df
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-09-11 13:06:33 +0100

    wasm components

--- hcase.c
+++ hcase.c
@@ -1,23 +1,19 @@
-//https://wasdk.github.io/WasmFiddle/
-#include<string.h>
-#include<stdio.h>
-  char i[]={"hello world...\n"};
-char *hello(){ 
-	char *ii;
-	ii=i;
-	return ii;
-}
+char i[1024]="hello world\n\0";
 char *hcases(){
 	int ff=0;
 	char c=0;
 	char *cc;
-	for(ff=0;ff<strlen(i);ff++){
+     do{
 		c=i[ff];
+		if(c==0)goto hcaseescape;
 		if(c>96 && c<123){
 			c=c-32;
 			i[ff]=c;
 		}
-	}
+		ff++;
+		
+	}while(1);
+	hcaseescape:
 	cc=i;
 	return cc;
 }

474579d32ce171d457a738d50f3b9fc5cf178537
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-09-11 13:59:14 +0100

    wasm sample

--- llists.c
+++ llists.c
@@ -1,33 +1,34 @@
 //https://wasdk.github.io/WasmFiddle/
-#include<string.h>
-#include<stdio.h>
-  char i[1024];
-char *hello(){
-	i[0]=0; 
-	char *ii;
-	ii=i;
-	return ii;
-}
+char *strcopys(char *c,char *cc){
+int ff=0;
+char *ccc;
+	ccc=c+0;
+	for(ff=0;ff<6000;ff++){
+		
+		if (cc[ff]==0){
+			ccc=c+ff;
+			ff=7000;
+			}else{
+				c[ff]=cc[ff];
+				}
+	}
+
+	return ccc;
+	}
 char *llists(){
 	int ff=0;
 	char c=0;
 	char *cc;
-	char ii[50];
-	cc=i;
-	i[0]=0; 
-	strcat(cc,"\n<br>Title\n<br>");
+	char ii[5000];
+	cc=ii;
+	ii[0]=0; 
+	cc=strcopys(cc,"\n<br>Title\n<br>");
 	for(ff=0;ff<16;ff++){
-		sprintf(ii,"\n<br>%d item\n<br>",ff);
-		strcat(cc,ii);
+		cc=strcopys(cc,"\n<br>%d item\n<br>");
+		
 	}
+	cc[1]=0;
+	cc=ii;
 	return cc;
 }
 
-
-int main(){
-	int d=0;
-	int e=0;
-	printf("\033[42;37m\n");
-	puts(llists());
-	return 0;
-}

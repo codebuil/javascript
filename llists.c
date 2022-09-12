c29c8394c5ec04c7f460daa790eda45cf6d24cb0
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-09-12 18:13:02 +0100

    wasm sample

--- llists.c
+++ llists.c
@@ -1,29 +1,48 @@
 #include<stdio.h>
 char i[6000];
-int strcopys(char *c,int indexs){
-		int ii;
-		for (ii=0;ii<5000;ii++){
-			if(c[ii]==0){
-				i[indexs+ii]=0;
-				return indexs+ii;
-			}else{
-				i[indexs+ii]=c[ii];
-			}
-		}
-		return indexs;
-}
 char *llists(){
+	char *c;
+	char *c1="\n<br>";
+	char *c4=" item.\n<br>";
+	int ii;
 	int indexs=0;
 	char r[20]="0\0\0";
 	char *cc;
 	int f;
 	i[0]=0;
 	for(f=0;f<16;f++){
-		indexs=strcopys("\n<br>",indexs);
+		c=c1;
+		for (ii=0;ii<5000;ii++){
+			if(c[ii]==0){
+				i[indexs+ii]=0;
+				indexs=indexs+ii;
+				ii=6000;
+			}else{
+				i[indexs+ii]=c[ii];
+			}
+		}
 		r[0]='0'+f;
 		if(f>9)r[0]='A'+f-10;
-		indexs=strcopys(r,indexs);
-		indexs=strcopys(" item.\n<br>",indexs);
+		c=r;
+		for (ii=0;ii<5000;ii++){
+			if(c[ii]==0){
+				i[indexs+ii]=0;
+				indexs=indexs+ii;
+				ii=6000;
+			}else{
+				i[indexs+ii]=c[ii];
+			}
+		}
+		c=c4;
+		for (ii=0;ii<5000;ii++){
+			if(c[ii]==0){
+				i[indexs+ii]=0;
+				indexs=indexs+ii;
+				ii=6000;
+			}else{
+				i[indexs+ii]=c[ii];
+			}
+		}
 	}
 	cc=i;
 	return cc;
